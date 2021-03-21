743eeb824b581ae76713c23c76ad80d95d39c94b
brunoemferreira <brunoemf@gmail.com>
2021-03-21 18:32:42 -0300

    creating instructors route

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require('./routes')
 
+// Inicializando o servidor
 const server = express()
 
+// mostra onde fica os arquivos estáticos
 server.use(express.static('public'))
+
+// Criando as rotas 
 server.use(routes)
 
+// Configuração da View Engine ( nunjucks )
 server.set("view engine", "njk")
-
 nunjucks.configure("views", {
   express: server,
   autoescape: false,
   noCache: true
 })
 
-
+// Colocando o servidor online
 server.listen(5000, function () {
   console.log("server is running")
 })
